087ae3ece53c798ae5ca1875164ce9dd6b732a2d
Laertis Pappas <laertis.pappas@gmail.com>
2016-07-26 00:28:12 +0300

    Adds generator for user_notification_model

--- lib/generators/user_notification/notification/notification_generator.rb
+++ lib/generators/user_notification/notification/notification_generator.rb
@@ -13,9 +13,31 @@ module UserNotification
         migration_template "migration.rb", "db/migrate/create_#{notifications_table_name}.rb"
       end
 
+      def copy_files
+        copy_file "model.rb", model_path
+
+        if self.behavior == :invoke
+          inject_into_class(model_path, model_class_name, model_content)
+        end
+      end
+
       private
+      def model_class_name
+        "#{table_name.singularize}_notification".classify
+      end
+
       def user_model_name
         UserNotification.configuration.user_class_name.downcase
+      end
+
+      def model_path
+        "app/models/#{table_name.singularize}_notification.rb"
+      end
+
+      def model_content
+        <<-TEXT
+  belongs_to #{user_model_name}
+        TEXT
       end
     end
   end
---
+++ lib/generators/user_notification/notification/templates/model.rb
@@ -0,0 +1,2 @@
+class UserNotification
+end
